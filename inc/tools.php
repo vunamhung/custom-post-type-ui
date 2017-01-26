0a7018eab29233d49e69f7b337dd2ca68677597c
Michael Beckwith <tw2113@gmail.com>
2017-01-26 00:35:03 -0600

    escape our translated text in the tools file

--- inc/tools.php
+++ inc/tools.php
@@ -193,7 +193,7 @@ function <?php echo $callback; ?>() {
 add_action( 'init', '<?php echo $callback; ?>' );
 <?php
 	} else {
-		_e( 'No taxonomies to display at this time', 'custom-post-type-ui' );
+		esc_html_e( 'No taxonomies to display at this time', 'custom-post-type-ui' );
 	}
 }
 
@@ -325,7 +325,7 @@ function <?php echo $callback; ?>() {
 add_action( 'init', '<?php echo $callback; ?>' );
 <?php
 	} else {
-		_e( 'No post types to display at this time', 'custom-post-type-ui' );
+		esc_html_e( 'No post types to display at this time', 'custom-post-type-ui' );
 	}
 }
 
@@ -647,14 +647,14 @@ function cptui_import_types_taxes_settings( $postdata = array() ) {
 function cptui_render_posttypes_taxonomies_section() {
 ?>
 
-	<p><?php _e( 'If you are wanting to migrate registered post types or taxonomies from this site to another, that will also use Custom Post Type UI, use the import and export functionality. If you are moving away from Custom Post Type UI, use the information in the "Get Code" tab.', 'custom-post-type-ui' ); ?></p>
+	<p><?php esc_html_e( 'If you are wanting to migrate registered post types or taxonomies from this site to another, that will also use Custom Post Type UI, use the import and export functionality. If you are moving away from Custom Post Type UI, use the information in the "Get Code" tab.', 'custom-post-type-ui' ); ?></p>
 
 <p>
 <?php
 	printf(
 		'<strong>%s</strong>: %s',
-		__( 'NOTE', 'custom-post-type-ui' ),
-		__( 'This will not export the associated posts or taxonomy terms, just the settings.', 'custom-post-type-ui' )
+		esc_html__( 'NOTE', 'custom-post-type-ui' ),
+		esc_html__( 'This will not export the associated posts or taxonomy terms, just the settings.', 'custom-post-type-ui' )
 	);
 ?>
 </p>
@@ -662,17 +662,17 @@ function cptui_render_posttypes_taxonomies_section() {
 	<?php if ( ! empty( $_GET ) && empty( $_GET['action'] ) ) { ?>
 		<tr>
 			<td class="outter">
-				<h2><label for="cptui_post_import"><?php _e( 'Import Post Types', 'custom-post-type-ui' ); ?></label></h2>
+				<h2><label for="cptui_post_import"><?php esc_html_e( 'Import Post Types', 'custom-post-type-ui' ); ?></label></h2>
 
 				<form method="post">
 					<textarea class="cptui_post_import" placeholder="<?php esc_attr_e( 'Paste content here.', 'custom-post-type-ui' ); ?>" id="cptui_post_import" name="cptui_post_import"></textarea>
 
 					<p class="wp-ui-highlight">
-						<strong><?php _e( 'Note:', 'custom-post-type-ui' ); ?></strong> <?php _e( 'Importing will overwrite previous registered settings.', 'custom-post-type-ui' ); ?>
+						<strong><?php esc_html_e( 'Note:', 'custom-post-type-ui' ); ?></strong> <?php esc_html_e( 'Importing will overwrite previous registered settings.', 'custom-post-type-ui' ); ?>
 					</p>
 
 					<p>
-						<strong><?php _e( 'To import post types from a different WordPress site, paste the exported content from that site and click the "Import" button.', 'custom-post-type-ui' ); ?></strong>
+						<strong><?php esc_html_e( 'To import post types from a different WordPress site, paste the exported content from that site and click the "Import" button.', 'custom-post-type-ui' ); ?></strong>
 					</p>
 
 					<p>
@@ -681,7 +681,7 @@ function cptui_render_posttypes_taxonomies_section() {
 				</form>
 			</td>
 			<td class="outter">
-				<h2><label for="cptui_post_export"><?php _e( 'Export Post Types', 'custom-post-type-ui' ); ?></label></h2>
+				<h2><label for="cptui_post_export"><?php esc_html_e( 'Export Post Types', 'custom-post-type-ui' ); ?></label></h2>
 				<?php
 				$cptui_post_types = cptui_get_post_type_data();
 				if ( ! empty( $cptui_post_types ) ) {
@@ -693,24 +693,24 @@ function cptui_render_posttypes_taxonomies_section() {
 				<textarea title="<?php esc_attr_e( 'To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'custom-post-type-ui' ); ?>" onclick="this.focus();this.select();" onfocus="this.focus();this.select();" readonly="readonly" aria-readonly="true" class="cptui_post_import" id="cptui_post_export" name="cptui_post_export"><?php echo $content; ?></textarea>
 
 				<p>
-					<strong><?php _e( 'Use the content above to import current post types into a different WordPress site. You can also use this to simply back up your post type settings.', 'custom-post-type-ui' ); ?></strong>
+					<strong><?php esc_html_e( 'Use the content above to import current post types into a different WordPress site. You can also use this to simply back up your post type settings.', 'custom-post-type-ui' ); ?></strong>
 				</p>
 			</td>
 		</tr>
 	<?php } elseif ( ! empty( $_GET ) && 'taxonomies' == $_GET['action'] ) { ?>
 		<tr>
 			<td class="outter">
-				<h2><label for="cptui_tax_import"><?php _e( 'Import Taxonomies', 'custom-post-type-ui' ); ?></label></h2>
+				<h2><label for="cptui_tax_import"><?php esc_html_e( 'Import Taxonomies', 'custom-post-type-ui' ); ?></label></h2>
 
 				<form method="post">
 					<textarea class="cptui_tax_import" placeholder="<?php esc_attr_e( 'Paste content here.', 'custom-post-type-ui' ); ?>" id="cptui_tax_import" name="cptui_tax_import"></textarea>
 
 					<p class="wp-ui-highlight">
-						<strong><?php _e( 'Note:', 'custom-post-type-ui' ); ?></strong> <?php _e( 'Importing will overwrite previous registered settings.', 'custom-post-type-ui' ); ?>
+						<strong><?php esc_html_e( 'Note:', 'custom-post-type-ui' ); ?></strong> <?php esc_html_e( 'Importing will overwrite previous registered settings.', 'custom-post-type-ui' ); ?>
 					</p>
 
 					<p>
-						<strong><?php _e( 'To import taxonomies from a different WordPress site, paste the exported content from that site and click the "Import" button.', 'custom-post-type-ui' ); ?></strong>
+						<strong><?php esc_html_e( 'To import taxonomies from a different WordPress site, paste the exported content from that site and click the "Import" button.', 'custom-post-type-ui' ); ?></strong>
 					</p>
 
 					<p>
@@ -719,7 +719,7 @@ function cptui_render_posttypes_taxonomies_section() {
 				</form>
 			</td>
 			<td class="outter">
-				<h2><label for="cptui_tax_export"><?php _e( 'Export Taxonomies', 'custom-post-type-ui' ); ?></label></h2>
+				<h2><label for="cptui_tax_export"><?php esc_html_e( 'Export Taxonomies', 'custom-post-type-ui' ); ?></label></h2>
 				<?php
 				$cptui_taxonomies = cptui_get_taxonomy_data();
 				if ( ! empty( $cptui_taxonomies ) ) {
@@ -731,7 +731,7 @@ function cptui_render_posttypes_taxonomies_section() {
 				<textarea title="<?php esc_attr_e( 'To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'custom-post-type-ui' ); ?>" onclick="this.focus();this.select()" onfocus="this.focus();this.select();" readonly="readonly" aria-readonly="true" class="cptui_tax_import" id="cptui_tax_export" name="cptui_tax_export"><?php echo $content; ?></textarea>
 
 				<p>
-					<strong><?php _e( 'Use the content above to import current taxonomies into a different WordPress site. You can also use this to simply back up your taxonomy settings.', 'custom-post-type-ui' ); ?></strong>
+					<strong><?php esc_html_e( 'Use the content above to import current taxonomies into a different WordPress site. You can also use this to simply back up your taxonomy settings.', 'custom-post-type-ui' ); ?></strong>
 				</p>
 			</td>
 		</tr>
@@ -749,12 +749,12 @@ function cptui_render_posttypes_taxonomies_section() {
  */
 function cptui_render_getcode_section() {
 ?>
-	<h1><?php _e( 'Get Post Type and Taxonomy Code', 'custom-post-type-ui' ); ?></h1>
+	<h1><?php esc_html_e( 'Get Post Type and Taxonomy Code', 'custom-post-type-ui' ); ?></h1>
 
-		<h2><?php _e( 'All CPT UI Post Types', 'custom-post-type-ui' ); ?></h2>
+		<h2><?php esc_html_e( 'All CPT UI Post Types', 'custom-post-type-ui' ); ?></h2>
 
 		<?php $cptui_post_types = cptui_get_post_type_data(); ?>
-		<label for="cptui_post_type_get_code"><?php _e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
+		<label for="cptui_post_type_get_code"><?php esc_html_e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
 		<textarea name="cptui_post_type_get_code" id="cptui_post_type_get_code" class="cptui_post_type_get_code" onclick="this.focus();this.select()" onfocus="this.focus();this.select();" readonly="readonly" aria-readonly="true"><?php cptui_get_post_type_code( $cptui_post_types ); ?></textarea>
 
 		<?php
@@ -762,16 +762,16 @@ function cptui_render_getcode_section() {
 			foreach ( $cptui_post_types as $post_type ) { ?>
 				<h2 id="<?php echo esc_attr( $post_type['name'] ); ?>"><?php
 					$type = ( ! empty( $post_type['label'] ) ) ? $post_type['label'] : $post_type['name'];
-					printf( __( '%s Post Type', 'custom-post-type-ui' ), $type ); ?></h2>
+					printf( esc_html__( '%s Post Type', 'custom-post-type-ui' ), $type ); ?></h2>
 				<label for="cptui_post_type_get_code_<?php echo $post_type['name']; ?>"><?php _e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
 				<textarea name="cptui_post_type_get_code_<?php echo $post_type['name']; ?>" id="cptui_post_type_get_code_<?php echo $post_type['name']; ?>" class="cptui_post_type_get_code" onclick="this.focus();this.select()" onfocus="this.focus();this.select();" readonly="readonly" aria-readonly="true"><?php cptui_get_post_type_code( array( $post_type ), true ); ?></textarea>
 			<?php }
 		} ?>
 
-		<h2><?php _e( 'All CPT UI Taxonomies', 'custom-post-type-ui' ); ?></h2>
+		<h2><?php esc_html_e( 'All CPT UI Taxonomies', 'custom-post-type-ui' ); ?></h2>
 
 		<?php $cptui_taxonomies = cptui_get_taxonomy_data(); ?>
-		<label for="cptui_tax_get_code"><?php _e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
+		<label for="cptui_tax_get_code"><?php esc_html_e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
 		<textarea name="cptui_tax_get_code" id="cptui_tax_get_code" class="cptui_tax_get_code" onclick="this.focus();this.select()" onfocus="this.focus();this.select();" readonly="readonly" aria-readonly="true"><?php cptui_get_taxonomy_code( $cptui_taxonomies ); ?></textarea>
 
 		<?php
@@ -779,8 +779,8 @@ function cptui_render_getcode_section() {
 			foreach ( $cptui_taxonomies as $taxonomy ) { ?>
 				<h2 id="<?php echo esc_attr( $taxonomy['name'] ); ?>"><?php
 					$tax = ( ! empty( $taxonomy['label'] ) ) ? $taxonomy['label'] : $taxonomy['name'];
-					printf( __( '%s Taxonomy', 'custom-post-type-ui' ), $tax ); ?></h2>
-				<label for="cptui_tax_get_code_<?php echo $taxonomy['name']; ?>"><?php _e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
+					printf( esc_html__( '%s Taxonomy', 'custom-post-type-ui' ), $tax ); ?></h2>
+				<label for="cptui_tax_get_code_<?php echo $taxonomy['name']; ?>"><?php esc_html_e( 'Copy/paste the code below into your functions.php file.', 'custom-post-type-ui' ); ?></label>
 				<textarea name="cptui_tax_get_code_<?php echo $taxonomy['name']; ?>" id="cptui_tax_get_code_<?php echo $taxonomy['name']; ?>" class="cptui_tax_get_code" onclick="this.focus();this.select()" onfocus="this.focus();this.select();" readonly="readonly" aria-readonly="true"><?php cptui_get_taxonomy_code( array( $taxonomy ), true ); ?></textarea>
 			<?php }
 		} ?>
@@ -806,7 +806,7 @@ function cptui_render_debuginfo_section() {
 		$debuginfo->send_email( $email_args );
 	}
 
-	echo '<p><label for="cptui_debug_info_email">' . __( 'Please provide an email address to send debug information to: ', 'custom-post-type-ui' ) . '</label><input type="email" id="cptui_debug_info_email" name="cptui_debug_info_email" value="" /></p>';
+	echo '<p><label for="cptui_debug_info_email">' . esc_html__( 'Please provide an email address to send debug information to: ', 'custom-post-type-ui' ) . '</label><input type="email" id="cptui_debug_info_email" name="cptui_debug_info_email" value="" /></p>';
 
 	/**
 	 * Filters the text value to use on the button when sending debug information.
